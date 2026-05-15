1c653db1525103b5c671172654cd68bdb1cdbfe4
Andy Baker <andy@andybak.net>
2026-05-15 19:52:23 +0100

    Forgot to give the preset a name [RELEASE]

--- src/lib/presets.ts
+++ src/lib/presets.ts
@@ -26,7 +26,7 @@ export const EXAMPLE_PRESETS: AppPreset[] = [
   { name: 'Pointy Grid', params: 'o=47wg.1b0y21'},
   { name: 'Maltese Floor', params: 'o=4ni8'},
   { name: 'Frutti', params: 't=15&cm=s&o=4zthc.1q1p24'},
-  { name: 'name', params: 't=y&p=alhambra&rf=0.6666666666666667&o=kd8g.1e0j0w'},
+  { name: 'Wakha', params: 't=y&p=alhambra&rf=0.6666666666666667&o=kd8g.1e0j0w'},
 ];
 
 export function getUserPresets(): AppPreset[] {
